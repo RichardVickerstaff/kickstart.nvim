3707071197a81bcba97373c14e52c225a68885e8
Richard Vickerstaff <richard.vickerstaff@carwow.co.uk>
2024-03-27 14:40:11 +0000

    Extract out some of the config

---
+++ lua/config/options.lua
@@ -0,0 +1,32 @@
+-- -- NOTE: This file is loaded BEFORE any plugins
+-- and should only be used to configure core neovim options
+
+-- Set leader outside of keymaps to ensure that plugins know what to bind to
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = false
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.scrolloff = 10
