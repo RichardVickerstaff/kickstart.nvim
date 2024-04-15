106744cd274ebfbc49953a729e6c1502e586edea
Richard Vickerstaff <richard.vickerstaff@carwow.co.uk>
2024-04-15 11:52:32 +0100

    Change rubocop command

--- lua/config/tests.lua
+++ lua/config/tests.lua
@@ -13,7 +13,7 @@ endfunction
 
 
 -- F3 to run rubocop
-vim.keymap.set('n', '<F3>', ':call InvokeViaTmux("carwow run rubocop -a", expand("%"))<CR>', { desc = 'Run Rubocop' })
+vim.keymap.set('n', '<F3>', ':call InvokeViaTmux("carwow run rubocop -A", expand("%"))<CR>', { desc = 'Run Rubocop' })
 
 vim.keymap.set('n', '<leader>t', ':call InvokeViaTmux("carwow run rspec", expand("%"))<CR>', { desc = 'Test file' })
 vim.keymap.set('n', '<leader>T', ':call InvokeViaTmux("carwow run rspec", expand("%") . ":" . line("."))<CR>', { desc = 'Test file' })
