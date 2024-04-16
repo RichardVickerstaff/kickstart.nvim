0fefa43399cd16384ffdacdaae5d070bd679451e
Richard Vickerstaff <richard.vickerstaff@carwow.co.uk>
2024-04-16 09:53:24 +0100

    Add CSS lint keybinding

--- lua/config/tests.lua
+++ lua/config/tests.lua
@@ -1,5 +1,5 @@
 vim.cmd [[
-function! InvokeViaTmux(cmd, test)
+function! InvokeViaTmuxWithFile(cmd, test)
   let l:targetWindow = trim(system('tmux list-windows | grep "test" | cut -f1 -d":" | head -1'))
   if empty(l:targetWindow)
     let l:targetWindow = 3
@@ -9,11 +9,52 @@ function! InvokeViaTmux(cmd, test)
   echom "Running " . a:test . " in window " . l:targetWindow
   let output = system(l:command)
 endfunction
+
+function! InvokeViaTmux(cmd)
+  let l:targetWindow = trim(system('tmux list-windows | grep "test" | cut -f1 -d":" | head -1'))
+  if empty(l:targetWindow)
+    let l:targetWindow = 3
+  endif
+  let l:target = "-t " . l:targetWindow . ".1"
+  let l:command = "tmux send-keys" . " " . l:target . ' "' . a:cmd . " "  . '" Enter'
+  echom "Running " . a:cmd . " in window " . l:targetWindow
+  let output = system(l:command)
+endfunction
 ]]
 
 
--- F3 to run rubocop
-vim.keymap.set('n', '<F3>', ':call InvokeViaTmux("carwow run rubocop -A", expand("%"))<CR>', { desc = 'Run Rubocop' })
-
 vim.keymap.set('n', '<leader>t', ':call InvokeViaTmux("carwow run rspec", expand("%"))<CR>', { desc = 'Test file' })
 vim.keymap.set('n', '<leader>T', ':call InvokeViaTmux("carwow run rspec", expand("%") . ":" . line("."))<CR>', { desc = 'Test file' })
+
+local aucmd_dict = {
+    FileType = {
+        {
+            pattern = "scss",
+            callback = function()
+                vim.api.nvim_set_keymap(
+                    'n',
+                    '<F3>',
+                    ':call InvokeViaTmux("carwow run yarn lint:css")<CR>',
+                    { desc = 'Run Rubocop' }
+                )
+            end,
+        },
+        {
+            pattern = "ruby",
+            callback = function()
+                vim.api.nvim_set_keymap(
+                    'n',
+                    '<F3>',
+                    ':call InvokeViaTmuxWithFile("carwow run rubocop -A", expand("%"))<CR>',
+                    { desc = 'Run Rubocop' }
+                )
+            end,
+        },
+    },
+}
+
+for event, opt_tbls in pairs(aucmd_dict) do
+    for _, opt_tbl in pairs(opt_tbls) do
+        vim.api.nvim_create_autocmd(event, opt_tbl)
+    end
+end
